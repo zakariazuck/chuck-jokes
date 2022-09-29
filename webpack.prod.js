8abcef1723c99cfde1600f445cd6e68521fbf510
zacksailor <zakarabenjawed@gmail.com>
2022-09-30 00:15:54 +0100

    Setup minifiers

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,13 @@
 const path = require("path");
 const common = require("./webpack.common");
 const {merge} = require("webpack-merge");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+
+const TerserPlugin = require("terser-webpack-plugin");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
@@ -13,11 +18,22 @@ module.exports = merge(common, {
   },
   optimization: {
     minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
       new HtmlWebpackPlugin({
         template: "./src/template.html",
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true
+        }
       })
     ]
   },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+    new CleanWebpackPlugin()
+  ],
   module: {
     rules: [
       {
