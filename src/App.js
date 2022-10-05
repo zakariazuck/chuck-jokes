7379caa56aaeaed61e2089e0f1605c516ec7ddfd
zacksailor <zakarabenjawed@gmail.com>
2022-10-05 20:30:10 +0100

    Create responsive typography system

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ const  App = () => {
 
     return (
       <div className="App">
+        <h1>Hi</h1>
         <div><a  class="text-white text-hover-white bg-brown-sorrell bg-hover-brown-sorrell-light" href="">Hi!!!</a></div>
+      <p>aloha!!</p>
       </div>
     )
   }
