f80ececf6fb7bf00f90ce6c22dae6822c6e943d8
zacksailor <zakarabenjawed@gmail.com>
2022-10-07 12:12:01 +0100

    Buttons

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ const  App = () => {
 
     return (
       <div className="App">
+        <button className=" btn-green-kiwi"> dont click  </button>
         <h1>Hi</h1>
-        <div><a  class="text-white text-hover-white bg-brown-sorrell bg-hover-brown-sorrell-light" href="">Hi!!!</a></div>
+        <div><a  className="text-white text-hover-white bg-brown-sorrell bg-hover-brown-sorrell-light" href="">Hi!!!</a></div>
       <p>aloha!!</p>
       </div>
     )
