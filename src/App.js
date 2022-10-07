43781a14c1fc18204e4f382f3ed0319511d89a21
zacksailor <zakarabenjawed@gmail.com>
2022-10-07 13:04:35 +0100

    Badges

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ const  App = () => {
 
     return (
       <div className="App">
+        
         <button className=" btn-green-kiwi"> dont click  </button>
+        <span className="badge-red"> Woow</span>
         <h1>Hi</h1>
         <div><a  className="text-white text-hover-white bg-brown-sorrell bg-hover-brown-sorrell-light" href="">Hi!!!</a></div>
       <p>aloha!!</p>
