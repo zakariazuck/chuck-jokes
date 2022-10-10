f1da25a84415309d63b9bf041bbb15ab6f15d5f3
zacksailor <zakarabenjawed@gmail.com>
2022-10-10 08:23:23 +0100

    Create nav component in sass

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ const  App = () => {
         <button className=" btn-green-kiwi"> dont click  </button>
         <span className="badge-red"> Woow</span>
         <h1>Hi</h1>
+        <nav className="navbar-black">
+          <div className="container justify-flex-end">
+            <ul className="display-f">
+              <li className="text-white-two text-hover-white-dark-1 bg-hover-black-light-1 p-2 "><a href="#">So funktioniert's</a></li>
+              <li className="text-white-two text-hover-white-dark-1 bg-hover-black-light-1 p-2"><a href="#">Sounderangebote</a></li>
+              <li className="text-white-two text-hover-white p-2 bg-brown-sorrell bg-hover-brown-sorrell-light-1">
+                  <a href="#">Mein Bereich</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
         <div><a  className="text-white text-hover-white bg-brown-sorrell bg-hover-brown-sorrell-light" href="">Hi!!!</a></div>
       <p>aloha!!</p>
       </div>
